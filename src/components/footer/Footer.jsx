d4ef38107a2d9a9c392e5fa6879a04b933d4fe1e
Jackson Smith <jackson16smith@gmail.com>
2023-05-09 22:30:43 -0400

    finished boiler plate and basic css features to scale extra additions

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,25 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Jackson</a>
+
+      <ul className="permalinks">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Jackson Smith. All rights reserved.</small>
+      </div>
+    </footer>
   )
 };
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './projects.css';
 
+const data = [
+  {
+    id: 1,
+    image: '',
+    title: '',
+    github: 'https://github.com',
+    demo: ''
+  },
+]
+
 const Projects = () => {
   return (
     <section id='projects'>
-      Projects
+      <h5>My Projects</h5>
+
+      <div className="container projects__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='projects__item'>
+                <div className="projects__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <a href={github} className='btn' target='_blank'>Source Code</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </article>
+            )
+          })
+        }
+      </div>
     </section>
   )
 };
